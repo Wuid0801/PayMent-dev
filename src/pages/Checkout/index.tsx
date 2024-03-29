aa5a65d7d189ba2b0ed51717fd6496695ffadf73
Wuid0801 <pig10326@naver.com>
2024-03-29 11:34:28 +0900

    [FIX] success api 변경, payments 이름 변경
    2024.03.29
    1. success페이지의 api 요청에 대한 404오류를 수정했습니다.
     1-1. fetch url을 수정했습니다.
    2. payments.js를 confirm.js로 변경하였습니다.

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
-import { loadPaymentWidget, ANONYMOUS } from "@tosspayments/payment-widget-sdk";
+import { loadPaymentWidget, ANONYMOUS, PaymentWidgetInstance } from "@tosspayments/payment-widget-sdk";
 import { nanoid } from "nanoid";
 
+// 구매자의 고유 아이디를 불러와서 customerKey로 설정하세요.
+// 이메일・전화번호와 같이 유추가 가능한 값은 안전하지 않습니다.
 const widgetClientKey = `${process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY}`;
 const customerKey = "2K4_8BsfT0OGhFKkgibnM";
+// const paymentWidget = PaymentWidget(widgetClientKey, PaymentWidget.ANONYMOUS) // 비회원 결제
 
 export default function CheckoutPage() {
-  const [paymentWidget, setPaymentWidget] = useState<any>(null);
+  const [paymentWidget, setPaymentWidget] = useState<PaymentWidgetInstance|null>(null);
   const paymentMethodsWidgetRef = useRef<any>(null);
-  const [price, setPrice] = useState(5_000);
+  const [price, setPrice] = useState(50_000);
 
   useEffect(() => {
     const fetchPaymentWidget = async () => {
       try {
-        const loadedWidget = await loadPaymentWidget(
-          widgetClientKey,
-          customerKey
-        );
+        const loadedWidget = await loadPaymentWidget(widgetClientKey, customerKey);
         setPaymentWidget(loadedWidget);
       } catch (error) {
         console.error("Error fetching payment widget:", error);
@@ -31,13 +31,18 @@ export default function CheckoutPage() {
       return;
     }
 
+    // 결제 ui
     const paymentMethodsWidget = paymentWidget.renderPaymentMethods(
       "#payment-widget",
       { value: price },
       { variantKey: "DEFAULT" }
     );
 
-    paymentWidget.renderAgreement("#agreement", { variantKey: "AGREEMENT" });
+    // 이용약관 ui
+    paymentWidget.renderAgreement(
+      "#agreement", 
+      { variantKey: "AGREEMENT" }
+    );
 
     paymentMethodsWidgetRef.current = paymentMethodsWidget;
   }, [paymentWidget, price]);
@@ -53,12 +58,15 @@ export default function CheckoutPage() {
   }, [price]);
 
   const handlePaymentRequest = async () => {
+
     try {
       await paymentWidget?.requestPayment({
         orderId: nanoid(),
-        amount: 5000,
-        orderName: "아이폰",
-        successUrl: `${window.location.origin}/success`,
+        orderName: "토스 티셔츠 외 2건",
+        customerName: "김토스",
+        customerEmail: "customer123@gmail.com",
+        customerMobilePhone: "01012341234",
+        successUrl: `${window.location.origin}/success?orderName=${encodeURIComponent("토스 티셔츠 외 2건")}`,
         failUrl: `${window.location.origin}/fail`,
       });
     } catch (error) {
@@ -68,6 +76,7 @@ export default function CheckoutPage() {
 
   return (
     <div>
+      {/* 할인 쿠폰 */}
       <label htmlFor="coupon-box">
         <input
           id="coupon-box"
@@ -78,8 +87,10 @@ export default function CheckoutPage() {
         />
         <span>5,000원 쿠폰 적용</span>
       </label>
+      {/* 결제 UI, 이용약관 UI 영역 */}
       <div id="payment-widget" />
       <div id="agreement" />
+      {/* 결제하기 버튼 */}
       <button onClick={handlePaymentRequest}>결제하기</button>
     </div>
   );
--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,54 +1,49 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-
-function Index({ searchParams }) {
+export default function SuccessPage() {
   const router = useRouter();
-  useEffect(() => {
-    if (!searchParams || !searchParams.orderId) {
-      // searchParams가 없거나 orderId가 없는 경우에 대한 처리
-      console.error('Missing orderId in searchParams');
-      return;
-    }
 
-    const { orderId, amount, orderName } = searchParams;
+  useEffect(() => {
+    console.log(router)
+    const { orderId, amount, paymentKey  } = router.query;
+
     const requestData = {
       orderId,
       amount,
-      orderName,
+      paymentKey,
     };
 
-    const url = `https://api.tosspayments.com/v1/payments/orders/${orderId}`;
-
     async function confirm() {
-      const response = await fetch(url, {
+      const response = await fetch("/api/confirm", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(requestData),
       });
-      console.log(response.body)
+      console.log(response)
       const json = await response.json();
 
       if (!response.ok) {
+        // 결제 실패 비즈니스 로직을 구현하세요.
         router.push(`/fail?message=${json.message}&code=${json.code}`);
         return;
       }
+
+      // 결제 성공 비즈니스 로직을 구현하세요.
     }
     confirm();
-  }, [searchParams, router]);
+  }, [router.query]);
 
   return (
     <div className="result wrapper">
       <div className="box_section">
         <h2>결제 성공</h2>
-
-        <p>{`주문번호: ${searchParams ? searchParams.orderId : ''}`}</p>
-        <p>{`결제 금액: ${Number(searchParams ? searchParams.amount : 0).toLocaleString()}원`}</p>
-        <p>{`orderName: ${searchParams ? searchParams.orderName : ''}`}</p>
+        <p>{`주문명: ${router.query.orderName}`}</p>
+        <p>{`주문번호: ${router.query.orderId}`}</p>
+        <p>{`결제 금액: ${Number(router.query.amount).toLocaleString()}원`}</p>
+        <p>{`paymentKey: ${router.query.paymentKey}`}</p>
       </div>
     </div>
   );
 }
-
-export default Index;
